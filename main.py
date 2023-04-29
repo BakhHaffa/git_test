7f9adcf40c5949b0ee8cdbbb971468d5f12ccd49
unknown <hafbakhpro@gmail.com>
2023-04-29 03:56:52 +0200

    main.py

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-a = int(input("number 1: "))
-b = int(input("number 2: "))
+a = int(input("number 1: ")) # La valeur de A sera renvoyée par l'input de l'utilisateur
+b = int(input("number 2: ")) # La valeur de B sera renvoyée par l'input de l'utilisateur
 
-def addition(a,b):
+def addition(a,b): # On definie la fonction addition
     sum=a+b
     return int(sum)
 
-def soustraction(a,b):
+def soustraction(a,b): # On definie la fonction soustraction
     sous=a-b
     return int(sous)
 
-def multiplication(a,b):
+def multiplication(a,b): # On definie la fonction multiplication
     times=a*b
     return int(times)
 
-def division(a,b):
+def division(a,b): # On definie la fonction division
     div=a/b
     return int(div)
 
